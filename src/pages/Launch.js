2e3b76cfd897f768a1a19b81969473a6bcbe54bf
imanuelcio <ciociocio009@gmail.com>
2024-05-08 13:05:43 +0700

    imprv: rename keplername

--- src/pages/Launch.js
+++ src/pages/Launch.js
@@ -5,8 +5,8 @@ import Clickable from "../components/Clickable";
 const Launch = (props) => {
   const selectorBody = useMemo(() => {
     return props.planets?.map((planet, i) => (
-      <option value={planet.kepler_name} key={i}>
-        {planet.kepler_name}
+      <option value={planet.keplerName} key={i}>
+        {planet.keplerName}
       </option>
     ));
   }, [props.planets]);
